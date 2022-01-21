2520836a3f3d02624761d5c849af98d636307767
adrgs <albastroiudragos@gmail.com>
2022-01-21 15:21:07 +0200

    Replaced Reactstrap with Tailwind

--- ClientApp/src/components/Layout.tsx
+++ ClientApp/src/components/Layout.tsx
@@ -1,16 +1,38 @@
 import * as React from 'react';
-import { Container } from 'reactstrap';
-import NavMenu from './NavMenu';
+import NavMenu, { HeaderLink } from './NavMenu';
+
+const links = [
+    new HeaderLink('Home', '/'),
+    new HeaderLink('Login', '/login'),
+    new HeaderLink('Counter', '/counter'),
+    new HeaderLink('Fetch data', '/fetch-data')
+];
 
 export default class Layout extends React.PureComponent<{}, { children?: React.ReactNode }> {
+
     public render() {
         return (
-            <React.Fragment>
-                <NavMenu />
-                <Container>
-                    {this.props.children}
-                </Container>
-            </React.Fragment>
+            <div className="mx-auto h-full" style={{ minHeight: 85 + 'vh' }}>
+                <div className="relative z-10 bg-white dark:bg-gray-800 overflow-hidden lg:w-full h-full">
+                    <main className="bg-gray-100 dark:bg-gray-800 h-screen overflow-hidden relative">
+                        <div>
+                            <NavMenu
+                                forceMenuOpenInMobile={false}
+                                forceDDMOpenInMobile={false}
+                                links={links}
+                                hideGitHubLink={true}
+                                hideHelp={true}
+                                alignRight={true}
+                            />
+                        </div>
+                        <div className="flex items-start justify-between">
+                            <div className="flex flex-col w-full md:space-y-4 pt-2">
+                                {this.props.children}
+                            </div>
+                        </div>
+                    </main>
+                </div>
+            </div>
         );
     }
 }
--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -1,10 +1,24 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+    "./src/*.{js,jsx,ts,tsx,html}",
   ],
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  important: true,
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
   plugins: [],
+  darkMode: 'class'
 }
