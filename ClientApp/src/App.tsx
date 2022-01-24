3ebc044fb8e89b33e853edee51ec71c4679533b7
adrgs <albastroiudragos@gmail.com>
2022-01-24 15:01:41 +0200

    Add 404 Page

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import Layout from './components/layout/Layout';
 import Home from './components/pages/Home';
 import Counter from './components/pages/Counter';
@@ -8,18 +8,22 @@ import Login from './components/pages/Login';
 import Signup from './components/pages/Signup';
 import ForgotPassword from './components/pages/ForgotPassword';
 import RecoverPassword from './components/pages/RecoverPassword';
+import NotFound from './components/pages/NotFound';
 
 import './index.generated.css'
 import './custom.css'
 
 export default () => (
     <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
-        <Route path='/forgotpassword' component={ForgotPassword} />
-        <Route path='/recoverpassword/:userid/:guid' component={RecoverPassword} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={Signup} />
+            <Route path='/forgotpassword' component={ForgotPassword} />
+            <Route path='/recoverpassword/:userid/:guid' component={RecoverPassword} />
+            <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+            <Route component={NotFound} />
+        </Switch>
     </Layout>
 );
---
+++ ClientApp/src/components/pages/NotFound.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => (
+  <>
+    <div className="flex relative z-20 items-center">
+      <div className="container mx-auto px-6 p-6 flex flex-col justify-between items-center relative py-8 bg-white dark:bg-gray-800 ">
+        <div className="flex flex-col">
+          <h1 className="font-light w-full uppercase text-center text-4xl sm:text-5xl dark:text-white text-gray-800">
+           ☹️ 404 Not Found 🚩
+          </h1>
+          <h2 className="font-light max-w-2xl mx-auto w-full text-xl dark:text-white text-gray-500 text-center py-8">
+            Go to the <Link to="/">Home page</Link>
+          </h2>
+        </div>
+      </div>
+    </div>
+
+  </>
+);
+
+export default connect()(NotFound);
