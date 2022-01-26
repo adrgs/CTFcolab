38a3aefc69716fb7ce89d3807b18b3e4cfc0e938
adrgs <albastroiudragos@gmail.com>
2022-01-26 13:25:54 +0200

    Add dark mode for profile page

--- ClientApp/src/components/misc/DescriptionList.tsx
+++ ClientApp/src/components/misc/DescriptionList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import _uniqueId from 'lodash/uniqueId';
 
 interface Props {
     dictObject: any
@@ -6,15 +7,15 @@ interface Props {
 
 const DescriptionList = (props: Props) => {
     return (
-        <div className="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg">
-            <div className="border-t border-gray-200">
+        <div className="bg-white dark:bg-gray-900 max-w-2xl shadow overflow-hidden sm:rounded-lg">
+            <div className="border-t border-gray-200 dark:border-black">
                 <dl>
-                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div key={_uniqueId('dl-')} className="bg-gray-50 dark:bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         {Object.keys(props.dictObject).map((key) => {
                             return (
                                 <>
-                                    <dt className="text-sm font-medium text-gray-500">{key}</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{props.dictObject[key]}</dd>
+                                    <dt className="text-sm font-medium text-gray-500 dark:text-white">{key}</dt>
+                                    <dd className="mt-1 text-sm text-gray-900 dark:text-white sm:mt-0 sm:col-span-2">{props.dictObject[key]}</dd>
                                 </>
                             )
                         })}
