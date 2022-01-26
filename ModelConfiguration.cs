c10fb7477de291d8e59f0b338fc1f44e7d8fdf6e
adrgs <albastroiudragos@gmail.com>
2022-01-26 06:06:41 +0200

    Migrate database with new tables

--- ModelConfiguration.cs
+++ ModelConfiguration.cs
@@ -10,6 +10,10 @@ namespace CTFcolab
             modelBuilder.Entity<User>().Property(u=>u.Email).IsRequired();
             modelBuilder.Entity<User>().Property(u=>u.Name).IsRequired();
             modelBuilder.Entity<User>().Property(u=>u.Password).IsRequired();
+            modelBuilder.Entity<Team>();
+            modelBuilder.Entity<Competition>();
+            modelBuilder.Entity<Comment>();
+            modelBuilder.Entity<Challenge>();
         }
     }
 }
