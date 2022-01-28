eca544685bd2e3b9f3b7ef6c7be758ee0360d31b
adrgs <albastroiudragos@gmail.com>
2022-01-28 06:50:49 +0200

    Implement password recovery through email

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -10,6 +10,9 @@ using System.Text;
 using Newtonsoft.Json;
 using CTFcolab.Helpers;
 using CTFcolab.Authorization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
 
 namespace CTFcolab.Controllers
 {
@@ -58,11 +61,13 @@ namespace CTFcolab.Controllers
         [ActionName("id")]
         public User UpdateId(int id, User user)
         {
-            try {
+            try
+            {
                 _userRepository.UpdateUser(user);
                 _userRepository.Save();
                 return user;
-            } catch {}
+            }
+            catch { }
             return null;
         }
 
@@ -70,11 +75,13 @@ namespace CTFcolab.Controllers
         [ActionName("id")]
         public ActionResult DeleteId(int id)
         {
-            try {
+            try
+            {
                 _userRepository.DeleteUser(id);
                 _userRepository.Save();
                 return Okay("User deleted");
-            } catch {}
+            }
+            catch { }
             return Bad("Couldn't delete user");
         }
 
@@ -86,6 +93,98 @@ namespace CTFcolab.Controllers
         private ActionResult Okay(object data)
         {
             return Ok(JsonConvert.SerializeObject(data));
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ActionName("recoverpassword")]
+        public ActionResult RecoverPassword(string id, User user)
+        {
+            if (user.Id == 0)
+            {
+                return Bad("User Id cannot be empty");
+            }
+            if (user.Password == null || user.Password == "")
+            {
+                return Bad("Password cannot be empty");
+            }
+
+            User userDb1 = _userRepository.GetUserByID(user.Id);
+
+            if (userDb1.ResetPasswordCode == null || userDb1.ResetPasswordCode.Code != id || userDb1.ResetPasswordCode.ExpirationDate < DateTime.Today)
+            {
+                return Bad("Invalid recovery code");
+            }
+
+            userDb1.ResetPasswordCode = null;
+            userDb1.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+            _userRepository.UpdateUser(userDb1);
+            _userRepository.Save();
+
+            return Ok("Password changed");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ActionName("forgotpassword")]
+        public ActionResult ForgotPassword(User user)
+        {
+            string username = user.Name;
+            string email = user.Email;
+            if (username == null || username.Length == 0)
+            {
+                return Bad("Username cannot be empty");
+            }
+            if (email == null || email.Length == 0)
+            {
+                return Bad("Email cannot be empty");
+            }
+            User userDb1 = _userRepository.GetUserByName(username);
+            User userDb2 = _userRepository.GetUserByEmail(email);
+            if (userDb1 == null || userDb2 == null || userDb1.Id != userDb2.Id)
+            {
+                return Bad("Invalid username/email");
+            }
+
+            ResetPasswordCode resetPasswordCode = new ResetPasswordCode();
+            resetPasswordCode.Code = Guid.NewGuid().ToString();
+            resetPasswordCode.ExpirationDate = DateTime.Today.AddDays(3);
+
+            userDb2.ResetPasswordCode = resetPasswordCode;
+
+            _userRepository.UpdateUser(userDb2);
+            _userRepository.Save();
+
+            try
+            {
+                string mailgunApi = MailgunConfig.MailgunApi;
+                string mailgunDomain = MailgunConfig.MailgunDomain;
+                using (var client = new HttpClient { BaseAddress = new Uri("https://api.mailgun.net/") })
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                        Convert.ToBase64String(Encoding.ASCII.GetBytes("api:" + mailgunApi)));
+
+                    var content = new FormUrlEncodedContent(new[]
+                    {
+                    new KeyValuePair<string, string>("from", "CTFcolab <mailgun@"+mailgunDomain+">"),
+                    new KeyValuePair<string, string>("to", userDb2.Email),
+                    new KeyValuePair<string, string>("subject", "[CTFcolab] Password recovery email"),
+                    new KeyValuePair<string, string>("html", "The following URL is valid for 3 days: <a href=\"" + "https://localhost:5001/recoverpassword/" + userDb2.Id + "/" + resetPasswordCode.Code +"\">https://localhost:5001/recoverpassword/" + userDb2.Id + "/" + resetPasswordCode.Code + "</a>")
+                });
+
+                    var task = Task.Run(() =>
+                        client.PostAsync("/v3/"+mailgunDomain+"/messages", content)
+                    );
+                    task.Wait();
+                    var response = task.Result;
+                    using var reader = new StreamReader(response.Content.ReadAsStream());
+                    var rsp = reader.ReadToEnd();
+                    return Ok("Email sent");
+                }
+            } catch {}
+
+            return Bad("Server error");
         }
 
         [HttpPost]
@@ -144,9 +243,12 @@ namespace CTFcolab.Controllers
                 {
                     _userRepository.InsertUser(user);
                     _userRepository.Save();
-                    if (user.Id == 1) {
+                    if (user.Id == 1)
+                    {
                         user.Role = "Admin";
-                    } else {
+                    }
+                    else
+                    {
                         user.Role = "User";
                     }
                     _userRepository.UpdateUser(user);
