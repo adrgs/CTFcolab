8494195f68e265cf1ef48abab0171eeca8959a76
adrgs <albastroiudragos@gmail.com>
2022-01-31 20:30:25 +0200

    Update DescriptionList.tsx

--- ClientApp/src/components/misc/DescriptionList.tsx
+++ ClientApp/src/components/misc/DescriptionList.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const DescriptionList = (props: Props) => {
     return (
-        <div className="bg-white dark:bg-gray-900 max-w-2xl shadow overflow-hidden sm:rounded-lg mb-6">
+        <div key={_uniqueId('dl-')} className="bg-white dark:bg-gray-900 max-w-2xl shadow overflow-hidden sm:rounded-lg mb-6">
             <div className="border-t border-gray-200 dark:border-black">
                 <dl>
                     <div key={_uniqueId('dl-')} className="bg-gray-50 dark:bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -16,12 +16,20 @@ const DescriptionList = (props: Props) => {
                             if (!props.keysWhitelist || props.keysWhitelist.includes(key)) {
                                 var val = props.dictObject[key];
                                 if (typeof val == "object") {
-                                    val = JSON.stringify(val);
+                                    val = JSON.parse(JSON.stringify(val));
+                                    if (Array.isArray(val)) {
+                                        val = val.map((el: any) => {
+                                            return el.name;
+                                        }).join('\n');
+                                    }
+                                    else {
+                                        val = val.name;
+                                    }
                                 }
                                 return (
                                     <>
-                                        <dt className="text-sm font-medium text-gray-500 dark:text-white">{key}</dt>
-                                        <dd className="mt-1 text-sm text-gray-900 dark:text-white sm:mt-0 sm:col-span-2">{val}</dd>
+                                        <dt key={_uniqueId('dt-')} className="text-sm font-medium text-gray-500 dark:text-white">{key}</dt>
+                                        <dd key={_uniqueId('dd-')} className="mt-1 text-sm text-gray-900 dark:text-white sm:mt-0 sm:col-span-2" style={{ whiteSpace: "pre-wrap" }}>{val}</dd>
                                     </>
                                 )
                             }
