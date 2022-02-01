b89ed986e3830575e0d55e4a599b1879ff08e60f
adrgs <albastroiudragos@gmail.com>
2022-02-01 23:41:48 +0200

    Add on delete cascade

--- ModelConfiguration.cs
+++ ModelConfiguration.cs
@@ -12,8 +12,11 @@ namespace CTFcolab
             modelBuilder.Entity<User>().Property(u=>u.Password).IsRequired();
             modelBuilder.Entity<Team>();
             modelBuilder.Entity<Competition>();
+            modelBuilder.Entity<Competition>().HasRequired(c => c.Team).WithMany().WillCascadeOnDelete(true);
             modelBuilder.Entity<Comment>();
+            modelBuilder.Entity<Comment>().HasRequired(c => c.Challenge).WithMany().WillCascadeOnDelete(true);
             modelBuilder.Entity<Challenge>();
+            modelBuilder.Entity<Challenge>().HasRequired(c => c.Competition).WithMany().WillCascadeOnDelete(true);
         }
     }
 }
