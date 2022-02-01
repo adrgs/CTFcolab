070144fa5bd778b96bec782bb5baa82b9971520a
adrgs <albastroiudragos@gmail.com>
2022-02-01 16:27:37 +0200

    Update Team view

--- ClientApp/src/components/pages/Team/Team.tsx
+++ ClientApp/src/components/pages/Team/Team.tsx
@@ -1,25 +1,39 @@
 import { inject, observer } from "mobx-react";
 import * as React from 'react';
+import Button from "../../misc/Button";
 import DescriptionList from "../../misc/DescriptionList";
 
-@inject("TeamStore")
+@inject("TeamStore", "UserStore")
 @observer
 export default class Team extends React.Component<any> {
     componentWillMount() {
 
         this.props.TeamStore.getTeam(this.props.match.params.teamid);
 
+        this.deleteTeam = this.deleteTeam.bind(this);
+        this.createCompetition = this.createCompetition.bind(this);
+
     }
 
     componentDidUpdate() {
         const { currentTeam } = this.props.TeamStore;
-        if (currentTeam.id.toString() != this.props.match.params.teamid) {
+        if (currentTeam && currentTeam.id.toString() != this.props.match.params.teamid) {
             this.props.TeamStore.getTeam(this.props.match.params.teamid);
         }
     }
 
+    deleteTeam() {
+        this.props.TeamStore.deleteTeam();
+        this.props.history.replace("/");
+    }
+
+    createCompetition() {
+        this.props.history.replace("/competition/create/" + this.props.match.params.teamid);
+    }
+
     render() {
         const { currentTeam } = this.props.TeamStore;
+        const { currentUser } = this.props.UserStore;
 
         if (currentTeam) {
             return (
@@ -29,7 +43,24 @@ export default class Team extends React.Component<any> {
                             <div className="flex flex-col">
                                 <DescriptionList dictObject={currentTeam} keysWhitelist={['id', 'name', 'inviteCode', 'owner', 'users', 'description']} />
                             </div>
-                            <h1>Competitions (WIP)</h1>
+                            {
+                                currentUser && currentTeam && currentUser.id == currentTeam.owner.id && 
+                                <div className="flex flex-col my-4">
+                                    <Button onClick={this.deleteTeam} label="Delete Team" color="red" />
+                                </div>
+                            }
+                            <h1>Competitions</h1>
+                            { (currentTeam && currentTeam.competitions && currentTeam.competitions > 0 &&
+                                <div className="flex flex-col">
+                                    <DescriptionList dictObject={currentTeam.competitions} />
+                                </div>) || (<h1><br></br>No competitions found.</h1>)
+                            }
+                            {
+                                currentUser && currentTeam && currentUser.id == currentTeam.owner.id && 
+                                <div className="flex flex-col my-4">
+                                    <Button onClick={this.createCompetition} label="Create competition" color="blue" />
+                                </div>
+                            }
                         </div>
                     </div>
                 </>
